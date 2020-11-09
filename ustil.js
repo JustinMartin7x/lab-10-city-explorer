b57e0d7b6eff531a214117796fe0ddd42d70bd39
JustinMartin7x <justin.martin7x@gmail.com>
2020-11-09 15:57:42 -0800

    committing work

---
+++ ustil.js
@@ -0,0 +1,8 @@
+function mungeLocation(location) {
+    return {
+        'formatted_query': location.display_name,
+        'latitude': location.lat,
+        'longitude': location.lon
+    };
+}
+module.exports = { mungeLocation }
